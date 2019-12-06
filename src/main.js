72daa4756a334d9039d3c460ab09fd87f1ac48e9
zhangkun <18864777510@163.com>
2019-12-06 10:47:04 +0800

    day6

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Swipe, SwipeItem } from 'vant';
+import 'vant/lib/index.css';
 
+Vue.use(Swipe).use(SwipeItem);
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-swipe :autoplay="3000" indicator-color="white">
+      <van-swipe-item class="item">1</van-swipe-item>
+      <van-swipe-item class="item">2</van-swipe-item>
+      <van-swipe-item class="item">3</van-swipe-item>
+      <van-swipe-item class="item">4</van-swipe-item>
+    </van-swipe>
+    <div class="box">
+        <div v-for="(item,index) in list" :key="index">{{item}}</div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+ data () {
+   return {
+     list:['成单奖励','成单奖励','成单奖励','成单奖励','成单奖励','成单奖励','成单奖励','成单奖励']
+   }
+ }
 }
 </script>
+<style lang="scss" scoped>
+.home{
+  width: 100%;
+  height: 200px;
+ 
+}
+.van-swipe {
+    position: relative;
+    overflow: hidden;
+    -webkit-user-select: none;
+    user-select: none;
+    width: 100%;
+    height: 200px;
+}
+.item{
+  width: 100%;
+  height: 200px;
+  background: skyblue;
+  text-align: center;
+  line-height: 200px;
+}
+.box{
+  width: 100%;
+  height: 200px;
+  display: flex;
+  flex-wrap: wrap;
+  div{
+    width: 25%;
+    line-height: 100px;;
+    text-align: center;
+  }
+}
+</style>
